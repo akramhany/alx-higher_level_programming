978fad6b54b737197b018cc00f82c248c5482fab
akramhany <akramhany65@gmail.com>
2024-01-25 13:57:43 -0800

    feat_Task10: Implement the check_cycle function

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,4 +1,6 @@
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
 * check_cycle - checks if a singly linked list contains a cycle or not
@@ -7,6 +9,45 @@
 */
 int check_cycle(listint_t *list)
 {
+	listint_t *begin = list;
+	listint_t *end = list;
+	listint_t *ptr = NULL;
 
+	if (!list)
+		return (0);
 
+	ptr = list->next;
+
+	while (ptr != NULL)
+	{
+		if (checkVisited(begin, end, ptr) == 1)
+		{
+			return (1);
+		}
+		end = ptr;
+		ptr = ptr->next;
+	}
+
+	return (0);
+}
+
+/**
+ * checkVisited - takes the start and end and check if the node was visited
+ * @begin: the start node.
+ * @end: the end node.
+ * @target: the node to search for.
+ * Return: 1 if it was visited, 0 if not.
+ */
+int checkVisited(listint_t *begin, listint_t *end, listint_t *target)
+{
+	listint_t *ptr = begin;
+
+	while (ptr != end)
+	{
+		if (ptr == target)
+			return (1);
+		ptr = ptr->next;
+	}
+
+	return (0);
 }
