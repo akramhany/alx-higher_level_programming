ec2e2f18a17c211ed659fa850e837991cab4b311
akramhany <akramhany65@gmail.com>
2024-01-25 20:49:46 +0000

    feat_Task10: Make the required file

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+* check_cycle - checks if a singly linked list contains a cycle or not
+* @list: the head of the list.
+* Return: 0 if no, 1 if yes.
+*/
+int check_cycle(listint_t *list)
+{
+
+
+}
