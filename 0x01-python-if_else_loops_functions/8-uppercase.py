6bec4379e3c7cb8cb0c6dbd57f95d2c75db03b59
akramhany <akramhany65@gmail.com>
2024-01-26 13:52:42 -0800

    fix_Task8: Change uppercase function to print uppercase instead of losercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 
 def uppercase(str):
     for char in str:
-        if ord(char) <= ord('Z') and ord(char) >= ord('A'):
-            char = chr(ord(char) + ord('a') - ord('A'))
+        if ord(char) <= ord('z') and ord(char) >= ord('a'):
+            char = chr(ord(char) - ord('a') + ord('A'))
         print('{}'.format(char), end='')
     print()
