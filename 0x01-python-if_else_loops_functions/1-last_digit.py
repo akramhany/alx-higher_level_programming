2dcd83d465da7fd2606d0349b109efa24bf74120
akramhany <akramhany65@gmail.com>
2024-01-26 11:07:55 -0800

    feat_Task1: Implement the required python script

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+lastDigit = number % 10
+stringToPrint = f"Last digit of {number} is {lastDigit} "
+
+if lastDigit > 5:
+    stringToPrint += "and is greater than 5"
+elif lastDigit == 0:
+    stringToPrint += "and is 0"
+else:
+    stringToPrint += "and is less than 6 and not 0"
+
+print(stringToPrint)
