a724444ca9357976c1aafa173007495469fa044a
akramhany <akramhany65@gmail.com>
2024-01-28 14:35:19 +0200

    fix_Task6: Change the match statements into if statements

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -10,16 +10,15 @@ if __name__ == '__main__':
     
     a, operator, b = int(argv[1]), argv[2], int(argv[3])
     result = 0
-    match operator:
-        case '+':
-            result = add(a, b)
-        case '-':
-            result = sub(a, b)
-        case '*':
-            result = mul(a, b)
-        case '/':
-            result = div(a, b)
-        case _:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
+    if operator == '+':
+        result = add(a, b)
+    elif operator == '-':
+        result = sub(a, b)
+    elif operator == '*':
+        result = mul(a, b)
+    elif operator == '/':
+        result = div(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
     print("{} {} {} = {}".format(a, operator, b, result))
