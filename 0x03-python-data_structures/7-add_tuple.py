072dc7b8d379fe53443ed01ec1b06417993c49d1
akramhany <akramhany65@gmail.com>
2024-01-29 21:49:26 +0200

    feat_Task implementation

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    first_element = 0
+    second_element = 0
+    if len(tuple_a) >= 2:
+        first_element += tuple_a[0]
+        second_element += tuple_a[1]
+    elif len(tuple_a) >= 1:
+        first_element += tuple_a[0]
+    
+    if len(tuple_b) >= 2:
+        first_element += tuple_b[0]
+        second_element += tuple_b[1]
+    elif len(tuple_b) >= 1:
+        first_element += tuple_b[0]
+    return (first_element, second_element)
