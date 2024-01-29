2812f5b80f8f9abaf218e83542c8ab2f2fc7c34f
akramhany <akramhany65@gmail.com>
2024-01-29 22:06:10 +0200

    fix_Task9: Handle the list containing negative numbers

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
-    maxInt = 0
+    maxInt = my_list[0]
     for integer in my_list:
         if integer > maxInt:
             maxInt = integer
