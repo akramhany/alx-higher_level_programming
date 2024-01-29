1a841b51a0b6e2d82322136a9a707949b640ee6f
akramhany <akramhany65@gmail.com>
2024-01-29 15:36:15 +0200

    fix_Task5: solve a styling error

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@
 def no_c(my_string):
     s = ''
     for char in my_string:
-        if not(char == 'c' or char == 'C'):
+        if not (char == 'c' or char == 'C'):
             s += char
     return s
