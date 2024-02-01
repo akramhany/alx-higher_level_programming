8c5151647ee635c22a04a5101a8da1e4c1245ceb
akramhany <akramhany65@gmail.com>
2024-02-01 19:03:30 +0200

    feat_Task6 implementation

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    a_list = sorted(a_dictionary)
+    for key in a_list:
+        print("{} : {}".format(key, a_dictionary.get(key)))
