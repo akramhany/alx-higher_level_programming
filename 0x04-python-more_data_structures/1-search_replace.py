7fa954e1b79ce240660a0be252c506ba09d5726f
akramhany <akramhany65@gmail.com>
2024-02-01 18:45:52 +0200

    feat_Task1 implementation

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    new_list = map(lambda item: (replace if item == search else item),my_list)
+    new_list = map(lambda item: (replace if item == search else item), my_list)
     return list(new_list)
