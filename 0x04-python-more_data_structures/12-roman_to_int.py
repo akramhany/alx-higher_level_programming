b695b8952e0ccbf05f7e460fb2e0c63dea92ece1
akramhany <akramhany65@gmail.com>
2024-02-01 20:33:29 +0200

    feat_Task12 implementation

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    lastChar = 'M'
+    number = 0
+    if roman_string is None:
+        return 0
+    for char in roman_string:
+        currentInt = romans[char]
+        lastInt = romans.get(lastChar)
+        if currentInt > lastInt:
+            number += (currentInt - 2 * lastInt)
+        else:
+            number += currentInt
+        lastChar = char
+    return number
