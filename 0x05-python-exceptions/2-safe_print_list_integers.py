2d861a86bcecb6212408ce960cc459083c5c9221
akramhany <akramhany65@gmail.com>
2024-02-09 00:51:05 +0200

    feat_Task2 implementation for 0x05 python exceptions

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
     num_of_ints = 0
     try:
         while i < x:
-            if type(my_list[i]) == int:
+            if isinstance(my_list[i], int):
                 print("{:d}".format(my_list[i]), end='')
                 num_of_ints += 1
             i += 1
