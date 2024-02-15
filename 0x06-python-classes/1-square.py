ab422090ac28e2aafa3507ca40121ae75e92779d
akramhany <akramhany65@gmail.com>
2024-02-15 19:47:59 +0200

    feat: Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""A module that contains the Square Class."""
+
+
+class Square:
+    """A class that represents a square."""
+    def __init__(self, size):
+        self.__size = size
