8977bf0bdeb2825d0e387e47efa79f0e5b5d84af
akramhany <akramhany65@gmail.com>
2024-02-20 17:18:47 +0200

    feat: add some more tests to task 6

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -10,6 +10,9 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([1, 5, 10, 8]), 10)
         self.assertIsNone(max_integer([]))
         self.assertEqual(max_integer([-123, -5, -1000, -1, -35]), -1)
+        self.assertEqual(max_integer([100, 1, 2]), 100)
+        self.assertEqual(max_integer([100, 1, 200]), 200)
+        self.assertEqual(max_integer([1]), 1)
 
     def test_type(self):
         self.assertRaises(TypeError, max_integer, 1)
