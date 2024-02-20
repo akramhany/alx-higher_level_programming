217c677921c2403a699179546ff14681cdf5e396
akramhany <akramhany65@gmail.com>
2024-02-20 14:34:57 +0200

    feat: task 3

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+""" this module contains the print_square function for task 3 in ALX """
+
+
+def print_square(size):
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        for j in range(size):
+            print("#", end='')
+        print()
