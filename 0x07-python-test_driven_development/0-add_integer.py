bd76fe14148d2c335b14452a9b6e676817c47b49
akramhany <akramhany65@gmail.com>
2024-02-20 11:29:52 +0200

    feat: Implement task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+""" A module that contains the add_integer function """
+
+
+def add_integer(a, b=98):
+    """
+    A function that takes 2 integers or floats (a, b) and returns their sum
+    """
+
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+
+    a = int(a)
+    b = int(b)
+
+    return a + b
