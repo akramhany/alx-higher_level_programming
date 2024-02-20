5c9338d01d3a2e372e5c9d2ac5cf6cf891eaa816
akramhany <akramhany65@gmail.com>
2024-02-20 17:16:10 +0200

    feat: Implement task 5

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+
+    def test_value(self):
+        self.assertEqual(max_integer([1, 5, 10, 8]), 10)
+        self.assertIsNone(max_integer([]))
+        self.assertEqual(max_integer([-123, -5, -1000, -1, -35]), -1)
+
+    def test_type(self):
+        self.assertRaises(TypeError, max_integer, 1)
+        self.assertRaises(TypeError, max_integer, ['akram', 1])
