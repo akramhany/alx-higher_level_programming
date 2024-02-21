4403e80f838c1f9aa31c59306e9d555902887d49
akramhany <akramhany65@gmail.com>
2024-02-21 16:16:35 +0200

    fix: tests

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -9,15 +9,17 @@ if len(sys.argv) != 2:
     print("Usage: nqueens N")
     exit(1)
 
-N = int(sys.argv[1])
+try:
+    N = int(sys.argv[1])
 
-if not isinstance(N, int):
+except ValueError:
     print("N must be a number")
     exit(1)
 
-#if N < 4:
-#    print("N must be at least 4")
-#    exit(1)
+
+if N < 4:
+    print("N must be at least 4")
+    exit(1)
 
 def verify_solution(sol, N):
     """ Takes a list of indecies, and verify it """
