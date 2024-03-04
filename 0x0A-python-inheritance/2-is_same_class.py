e0af92b3694c62f687f4f3a7552443ec083f902e
akramhany <akramhany65@gmail.com>
2024-03-04 11:07:31 +0200

    fix: pycode error for task 2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,12 +4,13 @@ a function that returns True if the object is exactly
 an instance of the specified class ; otherwise False.
 """
 
+
 def is_same_class(obj, a_class):
     """
     checks if an obj is an instance of a class
     """
 
-    if type(obj) == a_class:
+    if type(obj) is a_class:
         return True
     else:
         return False
