98803132a1734af8b88259bb9f86ee2e02c03201
akramhany <akramhany65@gmail.com>
2024-03-07 23:51:21 +0200

    feat: task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Contains Student class.
+"""
+
+
+class Student:
+    """
+    Represents a student.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        dt = self.__dict__
+
+        if attrs is None:
+            return dt
+
+        new_dt = {}
+        for attr in attrs:
+            if attr in dt:
+                new_dt[attr] = dt[attr]
+
+        return new_dt
+
+    def reload_from_json(self, json):
+        dt = self.__dict__
+
+        for attr in json:
+            dt[attr] = json[attr]
