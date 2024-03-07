3fa56366d8c8435aee9f7358310202d90feab677
akramhany <akramhany65@gmail.com>
2024-03-07 23:27:08 +0200

    feat: task 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -3,12 +3,10 @@
 A function that returns the dictionary description with simple data structure
 """
 
-import json
-
 
 def class_to_json(obj):
     """
     A function that returns the dictionary description of an object.
     """
 
-    return json.dumps(obj.__dict__)
+    return obj.__dict__
