329fdfedddecd396a3e94cb5af797ad7a75e30af
akramhany <akramhany65@gmail.com>
2024-03-08 00:54:58 +0200

    feat: task 12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# Pascal Triangal problem solution
+
+def pascal_triangle(n):
+    result = []
+ 
+    for i in range(n):
+        arr = []
+        if i == 0:
+            arr.append(1)
+        elif i == 1:
+            arr.append(1)
+            arr.append(1)
+        else:
+            arr.append(1)
+            for j in range(len(result[i - 1]) - 1):
+                arr.append(result[i - 1][j] + result[i - 1][j + 1])
+            arr.append(1)
+        result.append(arr)
+    return result
