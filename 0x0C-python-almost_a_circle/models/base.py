b1aa40a323c7961b59fce2ece20ba4ecc536999f
akramhany <akramhany65@gmail.com>
2024-03-08 01:59:43 +0200

    feat: task 1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+The base module in the package.
+"""
+
+
+class Base:
+    """
+    The base class of all other classes in the project, its goal
+    is to manage id attribute in all of the future classes.
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            __nb_objects += 1
+            self.id = __nb_objects
