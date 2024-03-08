e3b48737aee86703007d391e53521d1710882d5c
akramhany <akramhany65@gmail.com>
2024-03-08 17:14:34 +0200

    feat: task 12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -30,3 +30,40 @@ class Square(Rectangle):
     def size(self, val):
         self.width = val
         self.height = val
+
+    def __update_val(self, name, value):
+        """
+        Takes a name of an attribute and set it with a value.
+        """
+
+        if name == "id":
+            self.id = value
+        elif name == "size":
+            self.size = value
+        elif name == "x":
+            self.x = value
+        elif name == "y":
+            self.y = value
+
+    def update(self, *args, **kwargs):
+        """
+        Updage the square data with given arguments.
+        """
+
+        if len(args) == 0:
+            if kwargs is not None:
+                for key, value in kwargs.items():
+                    self.__update_val(key, value)
+                return
+
+        if len(args) > 0:
+            self.id = args[0]
+
+        if len(args) > 1:
+            self.size = args[1]
+
+        if len(args) > 2:
+            self.x = args[2]
+
+        if len(args) > 3:
+            self.y = args[3]
