fea963e3660eac5d8807a8d93112724a4f0d4315
akramhany <akramhany65@gmail.com>
2024-03-15 16:34:30 +0200

    feat: edit task 5 to take database name as an argument

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
 -- a script that prints the full description of the table first_table from the database hbtn_0c_0
-SHOW TABLE STATUS FROM hbtn_0c_0 LIKE 'first_table';
+SHOW TABLE STATUS LIKE 'first_table';
