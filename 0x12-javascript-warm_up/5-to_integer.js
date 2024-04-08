6b76233c3054350a0c1e1cf1c05eaa050dd70756
akramhany <akramhany65@gmail.com>
2024-04-08 22:03:26 +0200

    feat_Task5 implementation

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,8 +2,8 @@
 
 const { argv } = require('process');
 
-if (isNaN(parseInt(argv[2]))) {
+if (isNaN(+argv[2])) {
   console.log('Not a number');
 } else {
-  console.log(parseInt(argv[2]));
+  console.log(+argv[2]);
 }
