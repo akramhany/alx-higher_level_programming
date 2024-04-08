5b611e31b5f078109440ff9cdc54af23b75ae351
akramhany <akramhany65@gmail.com>
2024-04-08 23:29:39 +0200

    feat_Task11 implementation

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -9,6 +9,14 @@ if (argv.length === 2 || argv.length === 3) {
   for (let i = 2; i < argv.length; i++) {
     arr.push(+argv[i]);
   }
-  arr.sort();
-  console.log(arr[arr.length - 2]);
+  arr.sort(function (a, b) { return a - b; });
+  let i = arr.length - 1;
+  while (i >= 0 && arr[i] === arr[arr.length - 1]) {
+    i--;
+  }
+  if (i >= 0) {
+    console.log(arr[i]);
+  } else {
+    console.log(+'0');
+  }
 }
