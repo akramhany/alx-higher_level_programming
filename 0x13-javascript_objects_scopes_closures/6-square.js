579defae493e95d5fe94f23bb16424a304c78868
akramhany <akramhany65@gmail.com>
2024-04-10 04:10:46 +0200

    feat_Task6 implementation

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle');
+const OldSquare = require('./5-square');
 
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+class Square extends OldSquare {
   charPrint (c) {
     if (c === undefined) {
       this.print();
