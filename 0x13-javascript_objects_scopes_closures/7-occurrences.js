29db0a14ab4b0b1625709f49cb671f5b8ddb538a
akramhany <akramhany65@gmail.com>
2024-04-10 04:15:32 +0200

    feat_Task7 implementation

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  for (const num of list) {
+    if (num === searchElement) {
+      count += 1;
+    }
+  }
+
+  return count;
+};
