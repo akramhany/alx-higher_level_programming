1df63acf78a80c723d5a803854012f6bf8b99e83
akramhany <akramhany65@gmail.com>
2024-04-14 03:30:16 +0200

    feat_Task0 implementation

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,17 +5,17 @@
 import MySQLdb
 from sys import argv
 
-username = argv[1]
-password = argv[2]
-db_name = argv[3]
-
-db = MySQLdb.connect(
-    host='localhost', port=3306, user=username, passwd=password, db=db_name
-)
-
-cur = db.cursor()
 
 if __name__ == '__main__':
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+
+    db = MySQLdb.connect(
+        host='localhost', port=3306, user=username, passwd=password, db=db_name
+    )
+
+    cur = db.cursor()
     cur.execute('SELECT * FROM states ORDER BY states.id')
     results = cur.fetchall()
     for result in results:
