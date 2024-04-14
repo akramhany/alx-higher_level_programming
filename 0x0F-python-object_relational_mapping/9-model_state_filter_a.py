ce83cb18d9bdc60ac09deaac45d7ac484d505777
akramhany <akramhany65@gmail.com>
2024-04-14 16:10:47 +0200

    feat_Task9 implementation

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""A script that executes a query using sqlalchemy ORM"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+
+if __name__ == '__main__':
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+    engine = create_engine(
+        'mysql://{}:{}@localhost:3306/{}'.format(username, password, db_name)
+    )
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    results = (
+        session.query(State)
+        .filter(State.name.contains('a'))
+        .order_by(State.id)
+        .all()
+    )
+
+    for result in results:
+        print(str(result.id) + ': ' + result.name)
