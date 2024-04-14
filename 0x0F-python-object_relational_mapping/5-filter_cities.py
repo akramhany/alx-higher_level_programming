776a70ff59bfc461085a84bf02f00fa63d0def4a
akramhany <akramhany65@gmail.com>
2024-04-14 14:20:07 +0200

    feat_Task5 implementation

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""A simple script that connects to database and executes a query"""
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+    state_name = argv[4]
+
+    db = MySQLdb.connect(
+        host='localhost', port=3306, user=username, passwd=password, db=db_name
+    )
+
+    cur = db.cursor()
+    cur.execute(
+        'SELECT cities.id, cities.name, states.name FROM cities INNER JOIN \
+        states ON cities.state_id = states.id WHERE BINARY states.name = %s \
+        ORDER BY cities.id;',
+        (state_name,),
+    )
+    results = cur.fetchall()
+    for result in results:
+        print(result)
+
+    db.close()
