5f04d35d90b168dc6bc33486d1e361cd8b7149e6
akramhany <akramhany65@gmail.com>
2024-04-14 17:24:43 +0200

    feat_Task11 implementation

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""A script that executes a query using sqlalchemy ORM"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+
+if __name__ == '__main__':
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+
+    engine = create_engine(
+        'mysql://{}:{}@localhost:3306/{}'.format(username, password, db_name)
+    )
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    s = State(name='Louisiana')
+    session.add(s)
+    session.commit()
+    print(s.id)
