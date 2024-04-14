1f3df3d069d1789984afc49053d3a9575ff974a9
akramhany <akramhany65@gmail.com>
2024-04-14 03:34:05 +0200

    feat_Task1 implementation

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""A simple script that connects to database and executes a query"""
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+
+    db = MySQLdb.connect(
+        host='localhost', port=3306, user=username, passwd=password, db=db_name
+    )
+
+    cur = db.cursor()
+    cur.execute(
+        'SELECT * FROM states WHERE states.name LIKE "N%" ORDER BY states.id'
+    )
+    results = cur.fetchall()
+    for result in results:
+        print(result)
